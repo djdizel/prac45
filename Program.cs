8dc678bee45fe184b51530f4985be003d843844b
djdizel <yarmisikov@gmail.com>
2025-05-30 12:40:16 +0300

    123

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace prac45
     {
         static void Main(string[] args)
         {
+        Segment<int> segment1 = new Segment<int>(1, 1, 3, 3);
+        Segment<double> segment2 = new Segment<double>(1.5, 2.5, 3.5, 4.5);
+        Segment<uint> segment3 = new Segment<uint>(1, 2, 3, 4);
+
+        int intLength = (int)segment1.Length();
+        double doubleLength = segment2.Length();
+        uint uintLength = (uint)segment3.Length();
+
+        Console.WriteLine(segment1.ToString() + " Длина: " + intLength);
+        Console.WriteLine(segment2.ToString() + " Длина: " + doubleLength);
+        Console.WriteLine(segment3.ToString() + " Длина: " + uintLength);
         }
     }
 }
---
+++ Segment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac45
+{
+    class Segment<T>
+    {
+        public T x1 { get; set; }
+        public T y1 { get; set; }
+        public T x2 { get; set; }
+        public T y2 { get; set; }
+
+        public Segment(T x1, T y1, T x2, T y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public override string ToString()
+        {
+            return $"Отрезок: ({x1}, {y1}) до ({x2}, {y2})";
+        }
+        public double Length()
+        {
+            double dx = Convert.ToDouble(x2) - Convert.ToDouble(x1);
+            double dy = Convert.ToDouble(y2) - Convert.ToDouble(y1);
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
